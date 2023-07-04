68f46e8580ff8c2f1029bf6ba1312404c0fe60a8
Ralph Shui <rskc.2112@gmail.com>
2023-07-03 22:22:54 -0400

    completing props basic lab

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,23 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
+
+
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home 
+        name="Liza"
+        city="New York"
+        color="firebrick"
+      />
+      <About 
+        bio="I made this!"
+      />
     </div>
   );
 }
